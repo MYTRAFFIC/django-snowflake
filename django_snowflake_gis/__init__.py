5d99781679349404b187e49207183f2d9a09f2f5
Murat Cengiz <mcengiz2402@gmail.com>
2022-10-20 10:04:32 +0200

    __init__.py and package version

--- django_snowflake_gis/__init__.py
+++ django_snowflake_gis/__init__.py
@@ -1,3 +1,11 @@
+__version__ = '4.0a1'
+
+# Check Django compatibility before other imports which may fail if the
+# wrong version of Django is installed.
+from django_snowflake.utils import check_django_compatability
+
+check_django_compatability()
+
 from .functions import register_functions
 
 register_functions()
