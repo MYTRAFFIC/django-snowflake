cae1f549cc850dd9a1efdfea384856999e28c517
Murat Cengiz <mcengiz2402@gmail.com>
2022-11-08 16:23:18 +0100

    AsGeoJSON support

--- django_snowflake_gis/functions.py
+++ django_snowflake_gis/functions.py
@@ -1,4 +1,4 @@
-from django.contrib.gis.db.models.functions import Distance, Length, Perimeter
+from django.contrib.gis.db.models.functions import AsGeoJSON, Distance, Length, Perimeter
 from django_snowflake.functions import register_functions as snowflake_register_functions
 
 
@@ -11,9 +11,22 @@ def snowflake_distance(self, compiler, connection, **extra_context):
     )
 
 
+def snowflake_geojson(self, compiler, connection, **extra_context):
+    source_expressions = self.get_source_expressions()
+    clone = self.copy()
+    clone.set_source_expressions(source_expressions[:1])
+
+    function = connection.ops.spatial_function_name("ST_ASGEOJSON")
+
+    return super(AsGeoJSON, clone).as_sql(
+        compiler, connection, function=function, **extra_context
+    )
+
+
 def register_functions():
     snowflake_register_functions()
 
     Distance.as_snowflake = snowflake_distance
+    AsGeoJSON.as_snowflake = snowflake_geojson
     Length.as_snowflake = Length.as_postgresql
     Perimeter.as_snowflake = Perimeter.as_postgresql
