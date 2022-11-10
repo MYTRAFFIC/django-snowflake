b5f103c96cb0736e6a783f0da765f743bc7f1e33
Murat Cengiz <mcengiz2402@gmail.com>
2022-11-10 11:26:39 +0100

    Handle ST_Centroid function

--- django_snowflake_gis/functions.py
+++ django_snowflake_gis/functions.py
@@ -1,4 +1,4 @@
-from django.contrib.gis.db.models.functions import AsGeoJSON, Distance, Length, Perimeter
+from django.contrib.gis.db.models.functions import AsGeoJSON, Centroid, Distance, Length, Perimeter
 from django_snowflake.functions import register_functions as snowflake_register_functions
 
 
@@ -23,10 +23,22 @@ def snowflake_geojson(self, compiler, connection, **extra_context):
     )
 
 
+def snowflake_centroid(self, compiler, connection, **extra_context):
+    source_expressions = self.get_source_expressions()
+    clone = self.copy()
+    clone.set_source_expressions(source_expressions[:1])
+
+    function = connection.ops.spatial_function_name("ST_CENTROID")
+
+    return super(Centroid, clone).as_sql(
+        compiler, connection, function=function, **extra_context
+    )
+
 def register_functions():
     snowflake_register_functions()
 
     Distance.as_snowflake = snowflake_distance
     AsGeoJSON.as_snowflake = snowflake_geojson
+    Centroid.as_snowflake = snowflake_centroid
     Length.as_snowflake = Length.as_postgresql
     Perimeter.as_snowflake = Perimeter.as_postgresql
