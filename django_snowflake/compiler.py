b7ecb74d97aa992dbb7bdbe838f378e918779784
Murat Cengiz <mcengiz2402@gmail.com>
2022-11-15 11:55:12 +0100

    Generalize INSERT INTO SELECT format for all queries

--- django_snowflake/compiler.py
+++ django_snowflake/compiler.py
@@ -11,26 +11,25 @@ class SQLDeleteCompiler(compiler.SQLDeleteCompiler, SQLCompiler):
 
 class SQLInsertCompiler(compiler.SQLInsertCompiler, SQLCompiler):
     def as_sql(self):
-        if 'JSONField' in [field.get_internal_type() for field in self.query.fields]:
-            columns = [field.column for field in self.query.fields]
-            insert_columns = ", ".join([f'{column}' for column in columns])
-            insert_into = f'INSERT INTO {self.query.get_initial_alias()}({insert_columns})'
-            select_columns = ", ".join([
-                f"PARSE_JSON(Column{index + 1}) AS {field.column}"
-                if field.get_internal_type() == "JSONField"
-                else f"Column{index + 1} AS {field.column}"
-                for index, field in enumerate(self.query.fields)
-            ])
-            select = f'SELECT {select_columns}'
-            placeholders = f'({", ".join("%s" for field in self.query.fields)})'
-            from_values = f'FROM VALUES {", ".join([placeholders] * len(self.query.objs))}'
+        columns = [field.column for field in self.query.fields]
+        insert_columns = ", ".join([f'{column}' for column in columns])
+        insert_into = f'INSERT INTO {self.query.get_initial_alias()}({insert_columns})'
+        select_columns = ", ".join([
+            f"PARSE_JSON(Column{index + 1}) AS {field.column}"
+            if field.get_internal_type() == "JSONField"
+            else f"Column{index + 1} AS {field.column}"
+            for index, field in enumerate(self.query.fields)
+        ])
+        select = f'SELECT {select_columns}'
+        placeholders = f'({", ".join("%s" for field in self.query.fields)})'
+        from_values = f'FROM VALUES {", ".join([placeholders] * len(self.query.objs))}'
 
-            result = f"{insert_into} {select} {from_values}"
+        result = f"{insert_into} {select} {from_values}"
 
-            sql_with_params = self.query.sql_with_params()[0]
-            return [(result, sql_with_params[1])]
-
-        return super().as_sql()
+        rows = []
+        for obj in self.query.objs:
+            rows.extend([getattr(obj, column) for column in columns])
+        return [(result, rows)]
 
 
 class SQLUpdateCompiler(compiler.SQLUpdateCompiler, SQLCompiler):
