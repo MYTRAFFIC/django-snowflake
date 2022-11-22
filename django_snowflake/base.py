e116069bbe30be91645948fb0c36c90e85106d1e
Alexandre Peyrichout <apeyrichout@mytraffic.fr>
2022-11-22 14:32:04 +0100

    fix wrong parent class for SnowflakeCursorDebugWrapper.

--- django_snowflake/base.py
+++ django_snowflake/base.py
@@ -1,6 +1,6 @@
 from django.core.exceptions import ImproperlyConfigured
 from django.db.backends.base.base import BaseDatabaseWrapper
-from django.db.backends.utils import CursorWrapper, CursorDebugWrapper
+from django.db.backends.utils import CursorDebugWrapper, CursorWrapper
 from django.utils.asyncio import async_unsafe
 from psycopg2.sql import Composed
 
@@ -10,12 +10,12 @@ except ImportError as e:
     raise ImproperlyConfigured("Error loading snowflake connector module: %s" % e)
 
 # Some of these import snowflake connector, so import them after checking if it's installed.
-from .client import DatabaseClient                          # NOQA isort:skip
-from .creation import DatabaseCreation                      # NOQA isort:skip
-from .features import DatabaseFeatures                      # NOQA isort:skip
-from .introspection import DatabaseIntrospection            # NOQA isort:skip
-from .operations import DatabaseOperations                  # NOQA isort:skip
-from .schema import DatabaseSchemaEditor                    # NOQA isort:skip
+from .client import DatabaseClient  # NOQA isort:skip
+from .creation import DatabaseCreation  # NOQA isort:skip
+from .features import DatabaseFeatures  # NOQA isort:skip
+from .introspection import DatabaseIntrospection  # NOQA isort:skip
+from .operations import DatabaseOperations  # NOQA isort:skip
+from .schema import DatabaseSchemaEditor  # NOQA isort:skip
 
 
 class SnowflakeCursorWrapper(CursorWrapper):
@@ -25,7 +25,7 @@ class SnowflakeCursorWrapper(CursorWrapper):
         return super().execute(sql, params)
 
 
-class SnowflakeCursorDebugWrapper(CursorWrapper):
+class SnowflakeCursorDebugWrapper(CursorDebugWrapper):
     def execute(self, sql, params=None):
         if isinstance(sql, Composed):
             sql = composed_as_string(sql)
@@ -34,69 +34,71 @@ class SnowflakeCursorDebugWrapper(CursorWrapper):
 
 def composed_as_string(sql):
     if isinstance(sql, Composed):
-        return ''.join([composed_as_string(s) for s in sql.seq])
+        return "".join([composed_as_string(s) for s in sql.seq])
     return sql.string
 
 
 class DatabaseWrapper(BaseDatabaseWrapper):
-    vendor = 'snowflake'
-    display_name = 'Snowflake'
+    vendor = "snowflake"
+    display_name = "Snowflake"
     data_types = {
-        'AutoField': 'NUMBER(10,0)',
-        'BigAutoField': 'NUMBER(38,0)',
-        'BinaryField': 'BINARY',
-        'BooleanField': 'BOOLEAN',
-        'CharField': 'VARCHAR(%(max_length)s)',
-        'DateField': 'DATE',
-        'DateTimeField': 'TIMESTAMP_LTZ',
-        'DecimalField': 'NUMBER(%(max_digits)s,%(decimal_places)s)',
-        'DurationField': 'NUMBER(38,0)',
-        'FileField': 'VARCHAR(%(max_length)s)',
-        'FilePathField': 'VARCHAR(%(max_length)s)',
-        'FloatField': 'FLOAT',
-        'IntegerField': 'NUMBER(10,0)',
-        'BigIntegerField': 'NUMBER(38,0)',
-        'GenericIPAddressField': 'VARCHAR(39)',
-        'JSONField': 'VARIANT',
-        'NullBooleanField': 'BOOLEAN',
-        'OneToOneField': 'NUMBER(38,0)',
-        'PositiveBigIntegerField': 'NUMBER(38,0)',
-        'PositiveIntegerField': 'NUMBER(10,0)',
-        'PositiveSmallIntegerField': 'NUMBER(5,0)',
-        'SlugField': 'VARCHAR(%(max_length)s)',
-        'SmallAutoField': 'NUMBER(5,0)',
-        'SmallIntegerField': 'NUMBER(5,0)',
-        'TextField': 'VARCHAR',
-        'TimeField': 'TIME',
-        'UUIDField': 'VARCHAR(38)',
+        "AutoField": "NUMBER(10,0)",
+        "BigAutoField": "NUMBER(38,0)",
+        "BinaryField": "BINARY",
+        "BooleanField": "BOOLEAN",
+        "CharField": "VARCHAR(%(max_length)s)",
+        "DateField": "DATE",
+        "DateTimeField": "TIMESTAMP_LTZ",
+        "DecimalField": "NUMBER(%(max_digits)s,%(decimal_places)s)",
+        "DurationField": "NUMBER(38,0)",
+        "FileField": "VARCHAR(%(max_length)s)",
+        "FilePathField": "VARCHAR(%(max_length)s)",
+        "FloatField": "FLOAT",
+        "IntegerField": "NUMBER(10,0)",
+        "BigIntegerField": "NUMBER(38,0)",
+        "GenericIPAddressField": "VARCHAR(39)",
+        "JSONField": "VARIANT",
+        "NullBooleanField": "BOOLEAN",
+        "OneToOneField": "NUMBER(38,0)",
+        "PositiveBigIntegerField": "NUMBER(38,0)",
+        "PositiveIntegerField": "NUMBER(10,0)",
+        "PositiveSmallIntegerField": "NUMBER(5,0)",
+        "SlugField": "VARCHAR(%(max_length)s)",
+        "SmallAutoField": "NUMBER(5,0)",
+        "SmallIntegerField": "NUMBER(5,0)",
+        "TextField": "VARCHAR",
+        "TimeField": "TIME",
+        "UUIDField": "VARCHAR(38)",
     }
     data_types_suffix = {
-        'AutoField': 'AUTOINCREMENT',
-        'BigAutoField': 'AUTOINCREMENT',
-        'SmallAutoField': 'AUTOINCREMENT',
+        "AutoField": "AUTOINCREMENT",
+        "BigAutoField": "AUTOINCREMENT",
+        "SmallAutoField": "AUTOINCREMENT",
     }
     operators = {
-        'exact': '= %s',
-        'iexact': "ILIKE %s ESCAPE '\\\\'",
-        'contains': "LIKE %s ESCAPE '\\\\'",
-        'icontains': "ILIKE %s ESCAPE '\\\\'",
-        'gt': '> %s',
-        'gte': '>= %s',
-        'lt': '< %s',
-        'lte': '<= %s',
-        'startswith': "LIKE %s ESCAPE '\\\\'",
-        'endswith': "LIKE %s ESCAPE '\\\\'",
-        'istartswith': "ILIKE %s ESCAPE '\\\\'",
-        'iendswith': "ILIKE %s ESCAPE '\\\\'",
+        "exact": "= %s",
+        "iexact": "ILIKE %s ESCAPE '\\\\'",
+        "contains": "LIKE %s ESCAPE '\\\\'",
+        "icontains": "ILIKE %s ESCAPE '\\\\'",
+        "gt": "> %s",
+        "gte": ">= %s",
+        "lt": "< %s",
+        "lte": "<= %s",
+        "startswith": "LIKE %s ESCAPE '\\\\'",
+        "endswith": "LIKE %s ESCAPE '\\\\'",
+        "istartswith": "ILIKE %s ESCAPE '\\\\'",
+        "iendswith": "ILIKE %s ESCAPE '\\\\'",
     }
-    pattern_esc = r"REPLACE(REPLACE(REPLACE({}, '\\', '\\\\'), '%%', '\\%%'), '_', '\\_')"
+    pattern_esc = (
+        r"REPLACE(REPLACE(REPLACE({}, '\\', '\\\\'), '%%', '\\%%'), '_', '\\_')"
+    )
     pattern_ops = {
-        'contains': "LIKE '%%' || {} || '%%' ESCAPE '\\\\'",
-        'icontains': "ILIKE '%%' || {} || '%%' ESCAPE '\\\\'",
-        'startswith': "LIKE {} || '%%' ESCAPE '\\\\'",
-        'istartswith': "ILIKE {} || '%%' ESCAPE '\\\\'",
-        'endswith': "LIKE '%%' || {} ESCAPE '\\\\'",
-        'iendswith': "ILIKE '%%' || {} ESCAPE '\\\\'",
+        "contains": "LIKE '%%' || {} || '%%' ESCAPE '\\\\'",
+        "icontains": "ILIKE '%%' || {} || '%%' ESCAPE '\\\\'",
+        "startswith": "LIKE {} || '%%' ESCAPE '\\\\'",
+        "istartswith": "ILIKE {} || '%%' ESCAPE '\\\\'",
+        "endswith": "LIKE '%%' || {} ESCAPE '\\\\'",
+        "iendswith": "ILIKE '%%' || {} ESCAPE '\\\\'",
     }
 
     Database = Database
@@ -114,37 +116,37 @@ class DatabaseWrapper(BaseDatabaseWrapper):
     def get_connection_params(self):
         settings_dict = self.settings_dict
         conn_params = {
-            'interpolate_empty_sequences':  True,
-            **settings_dict['OPTIONS'],
+            "interpolate_empty_sequences": True,
+            **settings_dict["OPTIONS"],
         }
 
-        if settings_dict['NAME']:
-            conn_params['database'] = self.ops.quote_name(settings_dict['NAME'])
+        if settings_dict["NAME"]:
+            conn_params["database"] = self.ops.quote_name(settings_dict["NAME"])
 
-        if settings_dict['USER']:
-            conn_params['user'] = settings_dict['USER']
+        if settings_dict["USER"]:
+            conn_params["user"] = settings_dict["USER"]
         else:
-            raise ImproperlyConfigured(self.settings_is_missing % 'USER')
+            raise ImproperlyConfigured(self.settings_is_missing % "USER")
 
-        if settings_dict['PASSWORD']:
-            conn_params['password'] = settings_dict['PASSWORD']
-        elif 'authenticator' not in conn_params:
-            raise ImproperlyConfigured(self.settings_is_missing % 'PASSWORD')
+        if settings_dict["PASSWORD"]:
+            conn_params["password"] = settings_dict["PASSWORD"]
+        elif "authenticator" not in conn_params:
+            raise ImproperlyConfigured(self.settings_is_missing % "PASSWORD")
 
-        if settings_dict.get('ACCOUNT'):
-            conn_params['account'] = settings_dict['ACCOUNT']
+        if settings_dict.get("ACCOUNT"):
+            conn_params["account"] = settings_dict["ACCOUNT"]
         else:
-            raise ImproperlyConfigured(self.settings_is_missing % 'ACCOUNT')
+            raise ImproperlyConfigured(self.settings_is_missing % "ACCOUNT")
 
-        if settings_dict.get('WAREHOUSE'):
-            conn_params['warehouse'] = self.ops.quote_name(settings_dict['WAREHOUSE'])
+        if settings_dict.get("WAREHOUSE"):
+            conn_params["warehouse"] = self.ops.quote_name(settings_dict["WAREHOUSE"])
         else:
-            raise ImproperlyConfigured(self.settings_is_missing % 'WAREHOUSE')
+            raise ImproperlyConfigured(self.settings_is_missing % "WAREHOUSE")
 
-        if settings_dict.get('SCHEMA'):
-            conn_params['schema'] = self.ops.quote_name(settings_dict['SCHEMA'])
+        if settings_dict.get("SCHEMA"):
+            conn_params["schema"] = self.ops.quote_name(settings_dict["SCHEMA"])
         else:
-            raise ImproperlyConfigured(self.settings_is_missing % 'SCHEMA')
+            raise ImproperlyConfigured(self.settings_is_missing % "SCHEMA")
 
         return conn_params
 
@@ -171,7 +173,9 @@ class DatabaseWrapper(BaseDatabaseWrapper):
         if self.connection is None:
             return False
         with self.connection.cursor() as cursor:
-            conn_timezone_name = cursor.execute("SHOW PARAMETERS LIKE 'TIMEZONE'").fetchone()[1]
+            conn_timezone_name = cursor.execute(
+                "SHOW PARAMETERS LIKE 'TIMEZONE'"
+            ).fetchone()[1]
         timezone_name = self.timezone_name
         if timezone_name and conn_timezone_name != timezone_name:
             with self.connection.cursor() as cursor:
@@ -205,7 +209,7 @@ class DatabaseWrapper(BaseDatabaseWrapper):
         try:
             # Use a cursor directly, bypassing Django's utilities.
             with self.connection.cursor() as cursor:
-                cursor.execute('SELECT current_version()')
+                cursor.execute("SELECT current_version()")
         except Database.Error:
             return False
         else:
