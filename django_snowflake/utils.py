23228b53ee0bbffe02939d39cf32a842ca4df3cd
Carlos Navas <cnavas@mytraffic.fr>
2024-06-10 14:09:43 +0200

    Check only major version

--- django_snowflake/utils.py
+++ django_snowflake/utils.py
@@ -6,16 +6,15 @@ from django.utils.version import get_version_tuple
 def check_django_compatability():
     """
     Verify that this version of django-snowflake is compatible with the
-    installed version of Django. For example, any django-snowflake 3.2.x is
-    compatible with Django 3.2.y.
+    installed version of Django. For example, any django-snowflake 3.x is
+    compatible with Django 3.x.
     """
     from . import __version__
-    if django.VERSION[:2] != get_version_tuple(__version__)[:2]:
+    if django.VERSION[0] != get_version_tuple(__version__)[0]:
         raise ImproperlyConfigured(
-            'You must use the latest version of django-snowflake {A}.{B}.x '
-            'with Django {A}.{B}.y (found django-snowflake {C}).'.format(
+            'You must use the latest major version of django-snowflake {A}.x '
+            'with Django {A}.x (found django-snowflake {B}).'.format(
                 A=django.VERSION[0],
-                B=django.VERSION[1],
-                C=__version__,
+                B=__version__,
             )
         )
